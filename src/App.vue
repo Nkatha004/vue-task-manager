fb6455ffb58bafb051b771ede954ad05a4078358
Nkatha <nkathajoy36@gmail.com>
2024-01-16 13:51:22 +0300

    My vue app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header @toggle-add-task="toggleAddTask" title = 'Task Tracker' :showAddTask="showAddTask"/>
+    <router-view :showAddTask = "showAddTask"></router-view>
+    <Footer/>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
+  },
+  
+  data() {
+    return {
+      showAddTask: false
+    }
+  },
+  methods:{
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .container{
+    border: solid 1px;
+    padding: 0px 10px;
+  }
+
+  .btn{
+    background-color: black;
+    color: white;
+    padding: 10px;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+
+  .btn-block {
+    display: block;
+    width: 100%;
+    margin-top: 10px;
+  }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="centered">
+        <h1>About Us</h1>
+        <p>Version 1.0.0</p>
+        <router-link to = "/">Go back</router-link>
+    </div>
+    
+</template>
